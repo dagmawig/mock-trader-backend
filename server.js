40344d3ba75fda1ce4036cc2126ea682d2631918
Glitch (hello-express) <none>
2021-01-31 19:27:35 +0000

    💠😼 Checkpoint ./server.js:16901619/735 ./package.json:16901619/36

--- server.js
+++ server.js
@@ -5,7 +5,10 @@
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
+const cheerio = require("cheerio");
+const axios = require("axios");
 var cors = require('cors');
+
 
 const API_PORT = 3000;
 
@@ -14,10 +17,23 @@ const router = express.Router();
 app.use(cors({origin: '*'}));
 
 app.get('/getPrice/:ticker?', (req, res) => {
+        let url = 'https://finance.yahoo.com/quote/';
         var ticker = req.params.id;
+        url = url + ticker;
         
+  axios.get(url)
+    .then(res => {
+        const $ = cheerio.load(""+res.data);
+
+        let price = $('div[id="quote-header-info"]').find('span[class="Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"]').text().toString();
+
+        console.log(price);
+    })
+    .catch(err => {
+        console.log(err);
+    })
         })
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+
+// launch our backend into a port
+app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
+
