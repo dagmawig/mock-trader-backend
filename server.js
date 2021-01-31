6eb6f4a85c539f5d0ad12c4745847894a5365042
Glitch (hello-express) <none>
2021-01-31 23:34:34 +0000

    🍝😘 Checkpoint ./server.js:16901619/412

--- server.js
+++ server.js
@@ -18,10 +18,19 @@ const router = express.Router();
 
 app.use(cors({origin: '*'}));
 
-// (optional) only made for logging and
 // bodyParser, parses the request body to be a readable json format
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// connects our back end code with the database
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }); 
+
+let db = mongoose.connection;
+
+db.once('open', () => console.log('connected to database'));
+
+// checks if connection with the database is successful
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
