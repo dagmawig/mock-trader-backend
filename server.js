a8e83d7bcafe8d5dac17d553d9ed3d48b611f585
Glitch (hello-express) <none>
2021-02-06 01:20:34 +0000

    😼📇 Checkpoint ./server.js:16901619/6826

--- server.js
+++ server.js
@@ -7,17 +7,16 @@ const express = require("express");
 const app = express();
 const cheerio = require("cheerio");
 const axios = require("axios");
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const Data = require('./data');
-var cors = require('cors');
-
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const Data = require("./data");
+var cors = require("cors");
 
 const API_PORT = 3000;
 
 const router = express.Router();
 
-app.use(cors({origin: '*'}));
+app.use(cors({ origin: "*" }));
 
 // bodyParser, parses the request body to be a readable json format
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,76 +24,105 @@ app.use(bodyParser.json());
 
 // connects our back end code with the database
 //console.log(process.env.MONGO_URI)
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }); 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 let db = mongoose.connection;
 
-db.once('open', () => console.log('connected to database'));
+db.once("open", () => console.log("connected to database"));
 
 // checks if connection with the database is successful
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-
-
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
-
 
 // this is our create method
 // this method creates new user in our database
-router.post('/createUser', (req, res) => {
+router.post("/createUser", (req, res) => {
   let data = new Data();
   const { userID } = req.body;
   data.userID = userID;
-  
-  data.save((err) => {
-    if (err) return res.json({success: false, error: err});
-    return res.json({success: true, data: data});
+
+  data.save(err => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true, data: data });
   });
 });
 
 // this method updated stock watchlist
-router.post('/updateWatchlist', (req, res) => {
+router.post("/updateWatchlist", (req, res) => {
   const { userID, newWatchlist } = req.body;
   console.log(newWatchlist);
-  
-  Data.find( { userID: userID }, (err , data) => {
-    Data.findOneAndUpdate({ userID: userID }, { $set: {watchlist: newWatchlist} }, {new:true}, 
-                       (err, data) => {
-      if(err) throw err
-      console.log('Data after update: ', data);
-      return res.json({success: true, watchlist: data.watchlist})
+
+  Data.find({ userID: userID }, (err, data) => {
+    Data.findOneAndUpdate(
+      { userID: userID },
+      { $set: { watchlist: newWatchlist } },
+      { new: true },
+      (err, data) => {
+        if (err) throw err;
+        console.log("Data after update: ", data);
+        return res.json({ success: true, watchlist: data.watchlist });
+      }
+    );
+  });
+});
+
+router.get("/getPrice/:ticker?", (req, res) => {
+  let url = "https://finance.yahoo.com/quote/";
+  var ticker = req.params.ticker;
+  url = url + ticker;
+  console.log(url);
+
+  axios
+    .get(url)
+    .then(resp => {
+      const $ = cheerio.load("" + resp.data);
+      //console.log(resp.data);
+      let price = $('div[id="quote-header-info"]')
+        .find('span[class="Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"]')
+        .text()
+        .toString();
+
+      console.log("price is:", price);
+      return res.json({ success: true, price: price });
     })
-    
-    
-  } )
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 
-router.get('/getPrice/:ticker?', (req, res) => {
-        let url = 'https://finance.yahoo.com/quote/';
-        var ticker = req.params.ticker;
-        url = url + ticker;
-        console.log(url);
+router.get("/updatePrice", (req, res) => {
+  let url = "https://finance.yahoo.com/quote/";
+  const { searchT, watchlistT } = req.body;
+  let search = {ticker: searchT, price: ''};
+  let watchlist = {ticker: watchlistT, price: []};
   
-  axios.get(url)
+  axios
+    .get(url+searchT)
     .then(resp => {
-        const $ = cheerio.load(""+resp.data);
-        //console.log(resp.data);
-        let price = $('div[id="quote-header-info"]').find('span[class="Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"]').text().toString();
+      const $ = cheerio.load("" + resp.data);
+      let price = $('div[id="quote-header-info"]')
+        .find('span[class="Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"]')
+        .text()
+        .toString();
 
-        console.log("price is:", price);
-        return res.json({success: true, price: price});
+      console.log("price is:", price);
+    
+      //return res.json({ success: true, price: price });
     })
     .catch(err => {
-        console.log(err);
-    })
-        })
+      console.log(err);
+    });
+});
 
 // append /api for our http requests
-app.use('/', router);
+app.use("/", router);
 
 // launch our backend into a port
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
-
