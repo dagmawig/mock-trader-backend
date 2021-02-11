fb7a8438968c173649b453443be1bd0389ed0d44
Glitch (hello-express) <none>
2021-02-11 17:30:14 +0000

    🗾🚤 Checkpoint ./data.js:16901619/1557

--- data.js
+++ data.js
@@ -2,13 +2,22 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// this will be our database's data structure 
+// this will be our database's data structure
 const DataSchema = new Schema(
   {
     userID: { type: String, default: "" },
-    watchlist: { type: Object, default: {ticker: [], price: []} },
-    fund: 10000.00,
-    portfolio: {type: Schema.Types.Mixed, default: {}}
+    watchlist: { type: Object, default: { ticker: [] } },
+    fund:  { type: Number,  default: 10000 },
+    portfolio: {
+      type: Schema.Types.Mixed,
+      default: {
+        ticker: [],
+        shares: [],
+        purchaseP: [],
+        averageC: [],
+        purchaseD: []
+      }
+    }
   },
   { timestamps: true, _id: true, minimize: false, strict: false }
 );
