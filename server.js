fdd01cdc3d7b697858a143dd008055117827214a
Glitch (hello-express) <none>
2021-02-13 17:16:19 +0000

    🍁🙏 Checkpoint ./server.js:16901619/1907

--- server.js
+++ server.js
@@ -219,6 +219,53 @@ router.post("/buyTicker", (req, res) => {
           }
         }
       }
+      else {
+        if (shares * p > fund) {
+          return res.json({
+            success: false,
+            message: `Can not complete transaction! \nFunding $${formatNum(
+              fund
+            )} is not sufficient to buy ${shares} shares of ${ticker} at current price of $${price}!`
+          });
+        }
+        else {
+          fund = fund - shares * p;
+          let portfolio = data[0].portfolio;
+
+          if (!portfolio.ticker.includes(ticker.toUpperCase())) {
+            portfolio.ticker.push(ticker.toUpperCase());
+            portfolio.shares.push(shares);
+            portfolio.averageC.push(price);
+            let message = `Success! ${shares} shares of ${ticker.toUpperCase()} bought at a price of ${price}!`;
+            Data.findOneAndUpdate(
+              {userID: userID },
+              { $set: { portfolio: portfolio } },
+              { new: true },
+              (err, data) => {
+                if(err) throw err;
+                return res.json({ success: true, data: {data: data, message: message}});
+              }
+            )
+          }
+          else {
+            let index = portfolio.ticker.indexOf(ticker.toUpperCase());
+            let newShares = portfolio.shares[index] + shares;
+            let cost = (portfolio.shares[index]*portfolio.averageC[index] + shares*p)/(newShares);
+            portfolio.shares[index] = newShares;
+            portfolio.averageC[index] = cost;
+            let message = `Success! ${shares} shares of ${ticker.toUpperCase()} bought at a price of ${price}!`;
+            Data.findOneAndUpdate(
+              {userID: userID },
+              { $set: { portfolio: portfolio } },
+              { new: true },
+              (err, data) => {
+                if(err) throw err;
+                return res.json({ success: true, data: {data: data, message: message}});
+              }
+            )
+          }
+        }
+      }
     });
   });
 });
