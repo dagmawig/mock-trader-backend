9637332c3e602d6d020a80701493b51bd0d5ec5f
Glitch (hello-express) <none>
2021-03-30 02:21:05 +0000

    ⛸😚 Checkpoint ./server.js:16901619/37

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // connects our back end code with the database
-//console.log(process.env.MONGO_URI)
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
